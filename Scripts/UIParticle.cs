83ab8aedadaae5eca1e3d2fb4e434af694a2d61b
James Kim (Daewon) <eodnjs2998@gmail.com>
2025-08-11 14:30:40 +0900

    potential fix for focus regain crash.

--- Scripts/UIParticle.cs
+++ Scripts/UIParticle.cs
@@ -72,16 +72,16 @@ namespace Coffee.UIExtensions
         protected override void UpdateGeometry()
         {
 #if UNITY_EDITOR
-            if (_skipUpdate)
+            if (_skipUpdatePlayModeChanged)
             {
-                // #4  0x00000101d78ec4 in ParticleSystemRenderer::BakeMesh(PPtr<Mesh>, PPtr<Camera>, ParticleSystemBakeMeshOptions)
-                // ...
-                // #7  0x000004c70df968 in  Coffee.UIExtensions.UIParticleUpdater:BakeMesh (Coffee.UIExtensions.UIParticle,UnityEngine.Mesh) [{0x352dbded0} + 0x630] [/Users/jameskim/Develop/meow-tower/Packages/com.coffee.ui-particle/Scripts/UIParticleUpdater.cs :: 142u] (0x4c70df338 0x4c70e0238) [0x12f602a80 - Unity Child Domain]
-                // #8  0x000004c70dee30 in  Coffee.UIExtensions.UIParticleUpdater:Refresh () [{0x34d5de4b8} + 0x380] [/Users/jameskim/Develop/meow-tower/Packages/com.coffee.ui-particle/Scripts/UIParticleUpdater.cs :: 66u] (0x4c70deab0 0x4c70df058) [0x12f602a80 - Unity Child Domain]
-                // ...
-                // #25 0x000001021fd808 in PlayerLoopController::ExitPlayMode()
-                // #26 0x000001021f4260 in PlayerLoopController::SetIsPlaying(bool)
-                L.I("[UIParticle] Update() is skipped to prevent Unity crash.");
+                L.I("[UIParticle] Update() is skipped to prevent Unity crash on exiting play mode.");
+                return;
+            }
+
+            if (_skipUpdateFocusChanged)
+            {
+                _skipUpdateFocusChanged = false; // reset the flag.
+                L.I("[UIParticle] Update() is skipped once to prevent Unity crash on editor focus.");
                 return;
             }
 #endif
@@ -161,20 +161,85 @@ namespace Coffee.UIExtensions
         }
 
 #if UNITY_EDITOR
-        private static bool _skipUpdate;
+        private static bool _skipUpdatePlayModeChanged;
+        private static bool _skipUpdateFocusChanged;
 
-        static UIParticle() => UnityEditor.EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
-
-        private static void OnPlayModeStateChanged(UnityEditor.PlayModeStateChange state)
+        static UIParticle()
         {
-            if (state is UnityEditor.PlayModeStateChange.ExitingPlayMode)
+            /*
+               #4  0x00000101d78ec4 in ParticleSystemRenderer::BakeMesh(PPtr<Mesh>, PPtr<Camera>, ParticleSystemBakeMeshOptions)
+               ...
+               #7  0x000004c70df968 in  Coffee.UIExtensions.UIParticleUpdater:BakeMesh (Coffee.UIExtensions.UIParticle,UnityEngine.Mesh) [{0x352dbded0} + 0x630] [/Users/jameskim/Develop/meow-tower/Packages/com.coffee.ui-particle/Scripts/UIParticleUpdater.cs :: 142u] (0x4c70df338 0x4c70e0238) [0x12f602a80 - Unity Child Domain]
+               #8  0x000004c70dee30 in  Coffee.UIExtensions.UIParticleUpdater:Refresh () [{0x34d5de4b8} + 0x380] [/Users/jameskim/Develop/meow-tower/Packages/com.coffee.ui-particle/Scripts/UIParticleUpdater.cs :: 66u] (0x4c70deab0 0x4c70df058) [0x12f602a80 - Unity Child Domain]
+               ...
+               #25 0x000001021fd808 in PlayerLoopController::ExitPlayMode()
+               #26 0x000001021f4260 in PlayerLoopController::SetIsPlaying(bool)
+             */
+            UnityEditor.EditorApplication.playModeStateChanged += state =>
             {
-                _skipUpdate = true;
-            }
-            else if (state is UnityEditor.PlayModeStateChange.EnteredEditMode)
+                if (state is UnityEditor.PlayModeStateChange.ExitingPlayMode)
+                {
+                    _skipUpdatePlayModeChanged = true;
+                }
+                else if (state is UnityEditor.PlayModeStateChange.EnteredEditMode)
+                {
+                    _skipUpdatePlayModeChanged = false;
+                }
+            };
+
+            /*
+               0x349742fb8 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : mono_dump_native_crash_info
+               0x34970531c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : mono_handle_native_crash
+               0x34968da78 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : mono_sigsegv_signal_handler_debug
+               0x19cce96a4 - /usr/lib/system/libsystem_platform.dylib : _sigtramp
+               0x1032f8498 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN14StackAllocatorIL13AllocatorMode0EE13TryDeallocateEPv
+               0x1032e9de0 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN13MemoryManager10DeallocateEPvRK10MemLabelIdPKci
+               0x1032e81c8 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z19free_alloc_internalPvRK10MemLabelIdPKci
+               0x102e7c3d4 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN4core6vectorIhLm0EED2Ev
+               0x1041c0ec4 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN22ParticleSystemRenderer8BakeMeshE4PPtrI4MeshES0_I6CameraE29ParticleSystemBakeMeshOptions
+               0x10302dcac - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z38ParticleSystemRenderer_CUSTOM_BakeMeshP37ScriptingBackendNativeObjectPtrOpaqueS0_S0_29ParticleSystemBakeMeshOptions
+               0x4bb3babc4 - Unknown
+               ...
+               0x4c7f996fc - Unknown
+               0x349690d38 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : mono_jit_runtime_invoke
+               0x34981721c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : do_runtime_invoke
+               0x34981713c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/Frameworks/MonoBleedingEdge/MonoEmbedRuntime/osx/libmonobdwgc-2.0.dylib : mono_runtime_invoke
+               0x103a8bd54 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z23scripting_method_invoke18ScriptingMethodPtr18ScriptingObjectPtrR18ScriptingArgumentsP21ScriptingExceptionPtrb
+               0x103a6658c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN19ScriptingInvocation6InvokeEP21ScriptingExceptionPtrb
+               0x103ff5290 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN2UI13CanvasManager18WillRenderCanvasesEv
+               0x103ff779c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZZN2UI23InitializeCanvasManagerEvEN37UIEventsWillRenderCanvasesRegistrator7ForwardEv
+               0x1036b8e8c - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZZ23InitPlayerLoopCallbacksvEN45PostLateUpdatePlayerUpdateCanvasesRegistrator7ForwardEv
+               0x1036a04ac - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z17ExecutePlayerLoopP22NativePlayerLoopSystem
+               0x1036a04e0 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z17ExecutePlayerLoopP22NativePlayerLoopSystem
+               0x1036a08dc - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z10PlayerLoopv
+               0x1046441ac - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN16EditorPlayerLoop7ExecuteEv
+               0x104644998 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN20PlayerLoopController19InternalUpdateSceneEbb
+               0x10463ccd0 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN20PlayerLoopController31UpdateSceneIfNeededFromMainLoopEv
+               0x10463a140 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _ZN11Application9TickTimerEv
+               0x1058f1098 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : -[EditorApplication TickTimer]
+               0x19e3a2fcc - /System/Library/Frameworks/Foundation.framework/Versions/C/Foundation : __NSFireTimer
+               0x19cdb3c50 - /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation : __CFRUNLOOP_IS_CALLING_OUT_TO_A_TIMER_CALLBACK_FUNCTION__
+               0x19cdb3910 - /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation : __CFRunLoopDoTimer
+               0x19cdb344c - /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation : __CFRunLoopDoTimers
+               0x19cd99858 - /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation : __CFRunLoopRun
+               0x19cd98a98 - /System/Library/Frameworks/CoreFoundation.framework/Versions/A/CoreFoundation : CFRunLoopRunSpecific
+               0x1a883b27c - /System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/HIToolbox : RunCurrentEventLoopInMode
+               0x1a883e31c - /System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/HIToolbox : ReceiveNextEventCommon
+               0x1a89c9484 - /System/Library/Frameworks/Carbon.framework/Versions/A/Frameworks/HIToolbox.framework/Versions/A/HIToolbox : _BlockUntilNextEventMatchingListInModeWithFilter
+               0x1a0cbda34 - /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit : _DPSNextEvent
+               0x1a165c940 - /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit : -[NSApplication(NSEventRouting) _nextEventMatchingEventMask:untilDate:inMode:dequeue:]
+               0x1a0cb0be4 - /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit : -[NSApplication run]
+               0x1a0c872dc - /System/Library/Frameworks/AppKit.framework/Versions/C/AppKit : NSApplicationMain
+               0x105908ed0 - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : _Z10EditorMainiPPKc
+               0x1059091fc - /Applications/Unity/Hub/Editor/2022.3.60f1/Unity.app/Contents/MacOS/Unity : main
+             */
+            UnityEditor.EditorApplication.focusChanged += hasFocus =>
             {
-                _skipUpdate = false;
-            }
+                if (hasFocus)
+                {
+                    _skipUpdateFocusChanged = true;
+                }
+            };
         }
 #endif
     }
